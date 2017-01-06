ee49a8575b9a334b91abbb14fe0d61f91b4f003b
Yun Xu <yunx@zillowgroup.com>
2017-01-05 20:27:40 -0800

    add unit tests

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -3,6 +3,7 @@ import asyncio
 from migration.migration import Migration
 import pytest
 from pytest_lamp import asyncserver
+import whisper
 
 
 def test_migration_example():
@@ -39,3 +40,31 @@ async def test_graphite_connect():
     writer.close()
     assert message == data
     await worker.close_conn_to_graphite()
+
+
+@pytest.mark.asyncio
+async def test_read_from_wsps(monkeypatch):
+    loop = asyncio.get_event_loop()
+    worker = Migration('/opt/graphite/storage/whisper/zon',
+                       '127.0.0.1', 2003, loop=loop)
+    def fetch_mock_return(path, i):
+        return ((1483668388, 1483668392, 2), [7, 8])
+
+    monkeypatch.setattr(whisper, 'fetch', fetch_mock_return)
+
+    def walk_mock_return(path):
+        yield ('/opt/graphite/storage/whisper/zon', [], ['where.wsp'])
+    monkeypatch.setattr(os, 'walk', walk_mock_return)
+
+    await worker.read_from_wsps()
+
+    num = worker.queue.qsize()
+    # two datapoints and one terminator
+    assert num == 3
+    data1 = await worker.queue.get()
+    data2 = await worker.queue.get()
+    terminator = await worker.queue.get()
+    assert data1 == ('zon.where', 7, 1483668388)
+    assert data2 == ('zon.where', 8, 1483668390)
+    assert terminator == None
+
